597524d201e9da4a257c54bdf86222da10d89ecb
bovenson <szhkai@qq.com>
2017-07-27 17:41:55 +0800

    -

--- mindmap/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def return_404_page(request, *args, **kwargs):
-    return render(request, '404page.html')
---
+++ mindmap/views/views_index.py
@@ -0,0 +1,13 @@
+#!/usr/bin python3
+# coding: utf-8
+
+"""
+AUTHOR: bovenson
+EMAIL: szhkai@qq.com
+FILE: views_index.py
+DATE: 17-7-27 下午12:28
+DESC: 
+"""
+
+
+
