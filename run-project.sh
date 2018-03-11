7db2a23a7d82686ba0930fc49d6cc8a92b6e073c
bovenson <szhkai@qq.com>
2018-03-11 16:20:21 +0800

    auto commit

--- run-project.sh
+++ run-project.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/usr/bin/python3 manage.py runserver 0.0.0.0:9000 1>./other/log/stdout.log 2>./other/log/stderr.log &
+/usr/bin/python3 manage.py runserver 0.0.0.0:9000 1>/dev/null 2>/dev/null &
